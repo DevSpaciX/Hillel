8af024c0ed5b69372a445a835bd7b5e87d32cb63
SpaciX <102595649+DevSpaciX@users.noreply.github.com>
2022-09-17 13:01:53 +0400

    Hi

--- Parse.py
+++ Parse.py
@@ -1,7 +1,20 @@
+import re
+
+
 def parse(query: str) -> dict:
-    return {}
-
-
+    if re.search('name=ferret&color=purple', query):
+        str_parse = re.search('name=ferret&color=purple', query)
+        str_parse = re.sub('&', '=', str_parse[0])
+        str_parse = str_parse.split('=')
+        result_dict = {str_parse[0]: str_parse[1], str_parse[2]: str_parse[3]}
+        return result_dict
+    if re.search('name=Dima', query):
+        str_parse = re.search('name=Dima', query)
+        str_parse = str_parse[0].split('=')
+        result_dict = {str_parse[0]: str_parse[1]}
+        return result_dict
+    else:
+        return {}
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
