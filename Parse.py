f05cae49a1d00486f57580e42c1585aa3f4bc730
SpaciX <102595649+DevSpaciX@users.noreply.github.com>
2022-09-18 19:10:04 +0400

    Teacher help

--- Parse.py
+++ Parse.py
@@ -5,6 +5,7 @@ def parse(query: str) -> dict:
         return dict([i.split('=') for i in res if '=' in i])
     return {}
 
+
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
